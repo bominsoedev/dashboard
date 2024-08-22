f7ebcef430f5e7c725c637bd80fff26dd491818f
bominsoe.dev <bominsoe.mym@gmail.com>
2024-08-23 01:19:00 +0630

    Adding Destroy Section

--- resources/js/Components/MarkdownEditor.tsx
+++ resources/js/Components/MarkdownEditor.tsx
@@ -46,21 +46,19 @@ const markdownToHtml = (markdown: string) => {
                                         </button>
                                     </>) : (
                                         <>
-                                            <CopyToClipboard text={code} onCopy={() => setCopy(true)}>
-                                                <button
-                                                    className="flex gap-1 items-center">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                         fill="none" viewBox="0 0 24 24"
-                                                         className="size-4">
-                                                        <path fill="currentColor"
-                                                              fill-rule="evenodd"
-                                                              d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z"
-                                                              clip-rule="evenodd">
-                                                        </path>
-                                                    </svg>
-                                                    Copy code
-                                                </button>
-                                            </CopyToClipboard>
+                                            <button
+                                                className="flex gap-1 items-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                     fill="none" viewBox="0 0 24 24"
+                                                     className="size-4">
+                                                    <path fill="currentColor"
+                                                          fill-rule="evenodd"
+                                                          d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z"
+                                                          clip-rule="evenodd">
+                                                    </path>
+                                                </svg>
+                                                Copy code
+                                            </button>
                                         </>
                                     )
                                 }
--- resources/js/Pages/Article/Create.tsx
+++ resources/js/Pages/Article/Create.tsx
@@ -36,7 +36,7 @@ const Create = ({categories}: { categories: any }) => {
                 setTimeout(() => {
                     document.querySelectorAll('pre code').forEach((block) => {
                         hljs.highlightElement(block as HTMLElement);
-                    },0);
+                    }, 0);
                 })
             });
     };
@@ -154,21 +154,19 @@ const Create = ({categories}: { categories: any }) => {
                                             </button>
                                         </>) : (
                                             <>
-                                                <CopyToClipboard text={code} onCopy={() => setCopy(true)}>
-                                                    <button
-                                                        className="flex gap-1 items-center">
-                                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                                             fill="none" viewBox="0 0 24 24"
-                                                             className="size-4">
-                                                            <path fill="currentColor"
-                                                                  fill-rule="evenodd"
-                                                                  d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z"
-                                                                  clip-rule="evenodd">
-                                                            </path>
-                                                        </svg>
-                                                        Copy code
-                                                    </button>
-                                                </CopyToClipboard>
+                                                <button
+                                                    className="flex gap-1 items-center">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                         fill="none" viewBox="0 0 24 24"
+                                                         className="size-4">
+                                                        <path fill="currentColor"
+                                                              fill-rule="evenodd"
+                                                              d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z"
+                                                              clip-rule="evenodd">
+                                                        </path>
+                                                    </svg>
+                                                    Copy code
+                                                </button>
                                             </>
                                         )
                                     }
